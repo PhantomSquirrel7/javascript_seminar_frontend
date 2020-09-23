fa8775184b0f1edfc5c1e55c4adb9ed8665d9a6b
Lukas <l.hornik@tum.de>
2020-09-23 09:57:04 +0200

    Updated Login-Icon logic

--- src/app/components/landing-header/landing-header.component.ts
+++ src/app/components/landing-header/landing-header.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+import { User } from '@app/models';
+import { CustomUserService, CustomLoginService } from '@app/services/custom';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-landing-header',
   templateUrl: './landing-header.component.html',
   styleUrls: ['./landing-header.component.less']
 })
-export class LandingHeaderComponent implements OnInit {
+export class LandingHeaderComponent {
+  user: User;
+  error = '';
+  loggedin = false;
 
-  constructor() { }
+  constructor(private userService: CustomUserService, private _snackBar : MatSnackBar, private loginService : CustomLoginService) { }
 
   ngOnInit(): void {
+    this.loggedin = false;
+    this.userService.getMe().pipe(first()).subscribe({
+      next: (response) => {
+        this.loggedin = true;
+        this.user = response;
+      },
+      error: (error) => {
+        this.error = error;
+        this.loggedin = false;
+      },
+    })
   }
-
 }
