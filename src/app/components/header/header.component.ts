7ce66ac33704b9be73073ab47666961bc0ab04b7
Swornim Shrestha <minrows95@gmail.com>
2020-09-29 23:44:58 +0200

    [MODIFIED] Changes in Schema of Header Component (Usage of Bootstrap)

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.less']
 })
 export class HeaderComponent implements OnInit {
+  searchForm: FormGroup;
 
-  constructor() { }
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.searchForm = this.formBuilder.group({
+      searchText: '',
+    });
   }
+
+  // TODO: Implement Search function here
+  search() {}
 
 }
