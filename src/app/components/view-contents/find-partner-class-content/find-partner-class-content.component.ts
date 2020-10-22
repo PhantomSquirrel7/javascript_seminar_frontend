fd1a88ac52ac916df76d953ae414920ad0eae6d5
Lukas <l.hornik@tum.de>
2020-10-22 18:24:40 +0200

    Changed inputs of findpartnerclass to correspond with backend

--- src/app/components/view-contents/find-partner-class-content/find-partner-class-content.component.ts
+++ src/app/components/view-contents/find-partner-class-content/find-partner-class-content.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, SystemJsNgModuleLoader } from '@angular/core';
 import { User } from '@app/models';
 import { FormGroup, FormBuilder } from '@angular/forms'
 import { ClassesService } from 'src/app/services/swagger-api/classes.service'
+import { MetaService } from 'src/app/services/swagger-api/meta.service'
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -17,6 +18,7 @@ export class FindPartnerClassContentComponent implements OnInit {
 		private fb: FormBuilder,
 		private router: Router,
 		private classService: ClassesService,
+		private countryService: MetaService,
 		private _snackBar: MatSnackBar,
   	) { }
 
@@ -29,27 +31,50 @@ export class FindPartnerClassContentComponent implements OnInit {
 	error = '';
 	isSelected = false;
 	selectedClass: any; // Type Class
+	submitted = false;
+	classError = false;
 
 	user_classes = [];
 
-	user_lang_profs = ['Beginner (A1)', 'Beginner (A1)', 'Intermediate (B1)', 'Intermediate (B2)', 'Advanced (C1)', 'Advanced (C2)'];
+	user_lang_profs = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'];
 	selectedLangProf: string;
 	
-	user_duration = ['1 day', '1 week', '2 weeks', '3 weeks', '4 weeks +'];
-	selectedDuration: string;
+	// user_duration = ['1', '1 week', '2 weeks', '3 weeks', '4 weeks +'];
+	projectDuration: string = "";
+	
+	user_country = [];
+	selectedCountry: any;
 
 	myClasses:any = [];
 
 	resultClasses = [];
 
 	contactClass: any;
+
  
 	ngOnInit() {
 		this.findPartnerForm = this.fb.group({
 		selectedClass: [null],
 		selectedLangProf: [],
-		selectedDuration: []
+		projectDuration: [],
+		selectedCountry: [],
 		});
+
+		this.countryService.countriesGet().subscribe({
+			next: (response) => {
+				this.loading = false;
+				this.user_country = response;
+				this.selectedCountry = "";
+			},
+			error: (error) => {
+				this.error = error;
+				this._snackBar.open(this.error, 'Close', {
+				duration: 3000
+				});
+				this.loading = false;
+			},
+		});
+
 		this.classService.classesGet().subscribe({
 			next: (response) => {
 				this.loading = false;
@@ -75,34 +100,55 @@ export class FindPartnerClassContentComponent implements OnInit {
 		this.selectedLangProf = this.findPartnerForm.value.selectedLangProf;
 	}
 	
-	durationSelected(){
-		this.selectedDuration = this.findPartnerForm.value.selectedDuration;
+	// durationSelected(){
+	// 	this.selectedDuration = this.findPartnerForm.value.selectedDuration;
+	// }	
+	
+	countrySelected(){
+		console.log("Country:");
+		
+		console.log(this.findPartnerForm.value.selectedCountry);
+		this.selectedCountry = this.findPartnerForm.value.selectedCountry;
 	}
 
 	onSubmit() {
 		this.loading = true;
+		this.submitted = true;
 		
-		// this.router.navigate(['find-partner-class/results']);
+		if (this.selectedClass){
+			console.log("Hello Api:");
+			console.log(this.findPartnerForm.value);
+			console.log(this.selectedClass.id);
+			console.log(this.findPartnerForm.value.projectDuration);
+			console.log(this.selectedLangProf);
+			console.log(this.selectedCountry);
+			
+			// TODO: For production uncomment following line and comment out the line after
+			// this.myClasses = this.classService.classesClassIdFindGet(this.selectedClass.id, this.findPartnerForm.value.projectDuration, this.selectedLangProf, this.selectedCountry).subscribe({
+			this.myClasses = this.classService.classesGet().subscribe({
+				next: (response) => {
+					this.loading = false;
+					this.resultClasses = response;
+					console.log(this.resultClasses);
+					this.router.navigate(['find-partner-class/results'], {state: {data: response}});
+				},
+				error: (error) => {
+					this.error = error;
+					this._snackBar.open(this.error, 'Close', {
+					duration: 3000
+					});
+					this.loading = false;
+				},
+			});
+	
+			this.loading = false;
+		}
+		else{
+			console.log("ERROR!");
+			
+			this.classError = true;
+			this.loading = false;
+		}
 
-		console.log("Hello Api:");
-
-		// this.myClasses = this.classService.classesClassIdFindGet(this.selectedClass.id).subscribe({
-		this.myClasses = this.classService.classesGet().subscribe({
-			next: (response) => {
-				this.loading = false;
-				this.resultClasses = response;
-				console.log(this.resultClasses);
-				this.router.navigate(['find-partner-class/results'], {state: {data: response}});
-			},
-			error: (error) => {
-				this.error = error;
-				this._snackBar.open(this.error, 'Close', {
-				duration: 3000
-				});
-				this.loading = false;
-			},
-		});
-
-		this.loading = false;
 	}
 }
