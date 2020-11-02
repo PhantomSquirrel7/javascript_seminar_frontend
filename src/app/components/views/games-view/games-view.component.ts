f1d8af4dc4990afb011c83d28e77d50e3ab02392
ge52lof <irina.muntean@tum.de>
2020-11-02 18:47:38 +0100

    task list design only: student side (next to bbb frame)

--- src/app/components/views/games-view/games-view.component.ts
+++ src/app/components/views/games-view/games-view.component.ts
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesViewComponent implements OnInit {
 
+  title = "Task List";
+  // to be initialized in the constructor below - service needed
+  icebreakers = ["Ice-Breaker Game 1", "Ice-Breaker Game 2"];
+  quizzes = ["Quiz 1"];
+  others = ["Form 1"];
   constructor() { }
+
+  /*constructor(service : ){
+  }*/
 
   ngOnInit(): void {
   }
