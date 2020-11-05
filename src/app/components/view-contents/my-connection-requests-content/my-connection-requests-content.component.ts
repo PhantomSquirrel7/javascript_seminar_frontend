5699c63eb8c132288a6a903a744d37daada79bce
Lukas <l.hornik@tum.de>
2020-11-05 22:15:39 +0100

    filter for project creator works, wrong message loading bug fixed

--- src/app/components/view-contents/my-connection-requests-content/my-connection-requests-content.component.ts
+++ src/app/components/view-contents/my-connection-requests-content/my-connection-requests-content.component.ts
@@ -30,7 +30,6 @@ export class MyConnectionRequestsContentComponent implements OnInit {
   user: User;
   user_classes: any[];
   user_projects: any[] = [];
-  base_projects: any[] = [];
 
 
   actClass: any = {};
@@ -108,7 +107,6 @@ export class MyConnectionRequestsContentComponent implements OnInit {
                  return 0 
                 }  
               });
-              this.base_projects = [].concat(this.user_projects);
               })
             )
           )
@@ -135,38 +133,207 @@ export class MyConnectionRequestsContentComponent implements OnInit {
   }
 
   filterByCreator(creator){
-    var tmp = [].concat(this.base_projects);
-    console.log("TMP:");
-    console.log(tmp);
-    for (let clsProj of tmp){
-      clsProj.projects = clsProj.projects.filter( singleProj =>{
-        if (singleProj.startedBy != undefined){
-          if (creator == "me"){
-            return singleProj.startedBy.id == this.user.id;
-          }
-          else if (creator == "other"){
-            return singleProj.startedBy.id != this.user.id;
-          }
-          else {
-            return singleProj;
-          }
-        }
-        else{
-          if (creator == "me"){
-            return singleProj.partnerClass.id == clsProj.class.id;
-          }
-          else if (creator == "other"){
-            return singleProj.partnerClass.id != clsProj.class.id;
-          }
-          else {
-            return singleProj;
-          }
-        }
-      });
-    };
-    this.user_projects = [...tmp];
-    console.log("filtered: ");
-    console.log(tmp);
+    this.user_projects = [];
+      if (creator == "me"){
+          this.loadMyProjects();
+      }
+      else if (creator == "other"){
+          this.loadOtherProjects();
+      }
+      else {
+          this.loadAllProjects();
+      }
+      return true;
   }
   
+
+  loadMyProjects(){
+    this.classService.classesGet().pipe(
+      map( (classes) =>
+        classes.map( 
+          (cls) => this.projectService.classesClassIdProjectsGet(cls.id).subscribe(result => { // result = list of projects for one class
+            let clsProjects = [];
+            for (let project of result){
+              let partnerClass: any = {};
+              let modProject = project;
+              
+              for (let p of project.classes){
+                if (p.id != cls.id){
+                  partnerClass = p;
+                }  
+              }
+              if (project.startedBy != undefined){  // needed for possible errors on projects created before change in api
+                if (project.startedBy.id == this.user.id){
+                  modProject["partnerClass"] = partnerClass;
+                  clsProjects.push(modProject);
+                }
+              }
+              else{
+                if (partnerClass.id == cls.id){
+                    modProject["partnerClass"] = partnerClass;
+                    clsProjects.push(modProject);
+                }
+              }
+              
+            }
+            
+            this.user_projects.push({"class": cls, "projects": clsProjects});
+            // Sorting of the projects
+            this.user_projects = this.user_projects.sort( (a, b) => {
+              var clsNameA = a.class.name.toLowerCase();
+              var clsNameB = b.class.name.toLowerCase();
+              if (clsNameA < clsNameB){
+                return -1
+              }
+              else if (clsNameA > clsNameB){
+                return 1
+              }
+              else{
+               return 0 
+              }  
+            });
+            })
+          )
+        )
+        ).subscribe({
+            next: (response) => {
+              this.loading = false;
+              this.allDataLoaded = true;
+              console.log("all Projects:");
+              console.log(this.user_projects) 
+            },
+            error: (error) => {
+              this.error = error;
+              this._snackBar.open(this.error, 'Close', {
+              duration: 3000
+              });
+              this.loading = false;
+            },
+          });
+  }
+
+
+  loadOtherProjects(){
+    this.classService.classesGet().pipe(
+      map( (classes) =>
+        classes.map( 
+          (cls) => this.projectService.classesClassIdProjectsGet(cls.id).subscribe(result => { // result = list of projects for one class
+            let clsProjects = [];
+            for (let project of result){
+              let partnerClass: any = {};
+              let modProject = project;
+              
+              for (let p of project.classes){
+                if (p.id != cls.id){
+                  partnerClass = p;
+                }  
+              }
+
+              if (project.startedBy != undefined){  // needed for possible errors on projects created before change in api
+                if (project.startedBy.id != this.user.id){
+                  modProject["partnerClass"] = partnerClass;
+                  clsProjects.push(modProject);
+                }
+              }
+              else{
+                if (partnerClass.id != cls.id){
+                    modProject["partnerClass"] = partnerClass;
+                    clsProjects.push(modProject);
+                }
+              }             
+            }
+            
+            this.user_projects.push({"class": cls, "projects": clsProjects});
+            // Sorting of the projects
+            this.user_projects = this.user_projects.sort( (a, b) => {
+              var clsNameA = a.class.name.toLowerCase();
+              var clsNameB = b.class.name.toLowerCase();
+              if (clsNameA < clsNameB){
+                return -1
+              }
+              else if (clsNameA > clsNameB){
+                return 1
+              }
+              else{
+               return 0 
+              }  
+            });
+            })
+          )
+        )
+        ).subscribe({
+            next: (response) => {
+              this.loading = false;
+              this.allDataLoaded = true;
+              console.log("all Projects:");
+              console.log(this.user_projects) 
+            },
+            error: (error) => {
+              this.error = error;
+              this._snackBar.open(this.error, 'Close', {
+              duration: 3000
+              });
+              this.loading = false;
+            },
+          });
+  }
+
+
+  loadAllProjects(){
+    this.classService.classesGet().pipe(
+      map( (classes) =>
+        classes.map( 
+          (cls) => this.projectService.classesClassIdProjectsGet(cls.id).subscribe(result => {
+            let clsProjects = [];
+            for (let project of result){
+              let partnerClass = {};
+              let modProject = project;
+              
+              for (let p of project.classes){
+                if (p.id != cls.id){
+                  partnerClass = p;
+                }  
+              }
+              
+              modProject["partnerClass"] = partnerClass;
+              clsProjects.push(modProject);
+            }
+            this.user_projects.push({"class": cls, "projects": clsProjects});
+            // Sorting of the projects
+            this.user_projects = this.user_projects.sort( (a, b) => {
+              var clsNameA = a.class.name.toLowerCase();
+              var clsNameB = b.class.name.toLowerCase();
+              if (clsNameA < clsNameB){
+                return -1
+              }
+              else if (clsNameA > clsNameB){
+                return 1
+              }
+              else{
+               return 0 
+              }  
+            });
+            })
+          )
+        )
+        ).subscribe({
+            next: (response) => {
+              this.loading = false;
+              this.allDataLoaded = true;
+              console.log("all Projects:");
+              console.log(this.user_projects) 
+            },
+            error: (error) => {
+              this.error = error;
+              this._snackBar.open(this.error, 'Close', {
+              duration: 3000
+              });
+              this.loading = false;
+            },
+          });
+  }
+
+
 }
+
+
