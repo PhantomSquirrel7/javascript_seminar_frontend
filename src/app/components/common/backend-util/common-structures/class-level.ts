b74e199dae72de6d50c034bd1239b45b11fdb697
tarik <tariknural00@gmail.com>
2020-11-08 12:49:59 +0100

    class level, meeting freq. project duration naming convention

---
+++ src/app/components/common/backend-util/common-structures/class-level.ts
@@ -0,0 +1,12 @@
+export const CLASS_LEVEL = [
+  { name: 'Grade 4', value: '4' },
+  { name: 'Grade 5', value: '5' },
+  { name: 'Grade 6', value: '6' },
+  { name: 'Grade 7', value: '7' },
+  { name: 'Grade 8', value: '8' },
+  { name: 'Grade 9', value: '9' },
+  { name: 'Grade 10', value: '10' },
+  { name: 'Grade 11', value: '11' },
+  { name: 'Grade 12', value: '12' },
+  { name: 'Grade 13', value: '13' },
+];
---
+++ src/app/components/common/backend-util/common-structures/meeting-frequency.ts
@@ -0,0 +1,7 @@
+export const MEETING_FREQUENCY = [
+    { name: 'One Week', value: '1' },
+    { name: 'Two Weeks', value: '2' },
+    { name: 'One Moth', value: '4' },
+    { name: 'More than one month', value: '10' },
+  ];
+  
