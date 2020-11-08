c361eb6908fcfd59d4056b6a19fa90c1d0af1322
Lukas <l.hornik@tum.de>
2020-11-08 19:45:14 +0100

    still working on deployment

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/angular-9-jwt-refresh-tokens'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular-9-jwt-refresh-tokens/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
