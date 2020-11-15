a1bbe0aa71812420f621989cfe817aaaad130019
tarik <tariknural00@gmail.com>
2020-11-15 14:39:04 +0100

    mf,pd updates

--- src/app/components/common/backend-util/common-structures/meeting-frequency.ts
+++ src/app/components/common/backend-util/common-structures/meeting-frequency.ts
@@ -1,7 +1,6 @@
 export const MEETING_FREQUENCY = [
     { name: 'Once per week', value: '1' },
-    { name: 'Two times per week', value: '2' },
-    { name: 'Once a month', value: '4' },
-    { name: 'More than once in a month', value: '10' },
+    { name: 'Once in 2 weeks', value: '2' },
+    { name: 'Once per month', value: '4' }
   ];
   
--- src/app/components/common/backend-util/common-structures/project-duration.ts
+++ src/app/components/common/backend-util/common-structures/project-duration.ts
@@ -1,6 +1,7 @@
 export const PROJECT_DURATION = [
-    { name: 'One week', value: '1' },
-    { name: 'Two weeks', value: '2' },
+    { name: '1 week', value: '1' },
+    { name: '2 weeks', value: '2' },
     { name: 'One month', value: '4' },
+    { name: 'More than one month', value: '8' },
   ];
   
